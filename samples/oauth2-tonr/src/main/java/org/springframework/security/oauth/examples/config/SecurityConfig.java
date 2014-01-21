4ca05328e7fe9d23e7d554fd7e1f97218fa684b8
Dave Syer <dsyer@gopivotal.com>
2014-01-21 11:27:59 +0000

    Fix tonr2 client configuration

--- samples/oauth2-tonr/src/main/java/org/springframework/security/oauth/examples/config/SecurityConfig.java
+++ samples/oauth2-tonr/src/main/java/org/springframework/security/oauth/examples/config/SecurityConfig.java
@@ -1,87 +1,23 @@
 package org.springframework.security.oauth.examples.config;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import javax.annotation.Resource;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.client.token.AccessTokenRequest;
-import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
-import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
-import org.springframework.security.oauth2.common.AuthenticationScheme;
 import org.springframework.security.web.access.ExceptionTranslationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Value("${accessTokenUri}")
-	private String accessTokenUri;
-
-	@Value("${userAuthorizationUri}")
-	private String userAuthorizationUri;
-
-	@Resource
-	private AccessTokenRequest accessTokenRequest;
-
 	@Bean
 	public OAuth2ClientContextFilter oauth2ClientFilter() {
 		OAuth2ClientContextFilter filter = new OAuth2ClientContextFilter();
 		return filter;
-	}
-
-	@Bean
-	public OAuth2ProtectedResourceDetails sparklr() {
-		AuthorizationCodeResourceDetails details = new AuthorizationCodeResourceDetails();
-		details.setId("sparklr/tonr");
-		details.setClientId("tonr");
-		details.setClientSecret("secret");
-		details.setAccessTokenUri(accessTokenUri);
-		details.setUserAuthorizationUri(userAuthorizationUri);
-		details.setScope(Arrays.asList("read", "write"));
-		return details;
-	}
-
-	@Bean
-	public OAuth2ProtectedResourceDetails facebook() {
-		AuthorizationCodeResourceDetails details = new AuthorizationCodeResourceDetails();
-		details.setId("facebook");
-		details.setClientId("233668646673605");
-		details.setClientSecret("33b17e044ee6a4fa383f46ec6e28ea1d");
-		details.setAccessTokenUri("https://graph.facebook.com/oauth/access_token");
-		details.setUserAuthorizationUri("https://www.facebook.com/dialog/oauth");
-		details.setTokenName("oauth_token");
-		details.setClientAuthenticationScheme(AuthenticationScheme.form);
-		return details;
-	}
-
-	@Bean
-	public OAuth2ProtectedResourceDetails trusted() {
-		ClientCredentialsResourceDetails details = new ClientCredentialsResourceDetails();
-		details.setId("sparklr/trusted");
-		details.setClientId("my-client-with-registered-redirect");
-		details.setAccessTokenUri(accessTokenUri);
-		details.setScope(Arrays.asList("trust"));
-		return details;
 	}
 
 	@Override
@@ -116,47 +52,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordParameter("j_password")
                 .permitAll();
     	// @formatter:on
-	}
-
-	@Bean
-	public OAuth2RestTemplate facebookRestTemplate() {
-		OAuth2RestTemplate template = new OAuth2RestTemplate(facebook(), oauth2ClientContext());
-		MappingJacksonHttpMessageConverter converter = new MappingJacksonHttpMessageConverter();
-		converter
-				.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON, MediaType.valueOf("text/javascript")));
-		template.setMessageConverters(Arrays.<HttpMessageConverter<?>> asList(converter));
-		return template;
-	}
-
-	@Bean
-	public OAuth2RestTemplate sparklrRestTemplate() {
-		return new OAuth2RestTemplate(sparklr(), oauth2ClientContext());
-	}
-
-	@Bean
-	public OAuth2RestTemplate trustedClientRestTemplate() {
-		return new OAuth2RestTemplate(trusted());
-	}
-
-	@Bean
-	@Scope(value = "session", proxyMode = ScopedProxyMode.INTERFACES)
-	protected OAuth2ClientContext oauth2ClientContext() {
-		return new DefaultOAuth2ClientContext(accessTokenRequest);
-	}
-	
-	@Configuration
-	protected static class ClientContextConfiguration {
-
-		@Bean
-		@Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
-		protected AccessTokenRequest accessTokenRequest(@Value("#{request.parameterMap}")
-		Map<String, String[]> parameters, @Value("#{request.getAttribute('currentUri')}")
-		String currentUri) {
-			DefaultAccessTokenRequest request = new DefaultAccessTokenRequest(parameters);
-			request.setCurrentUri(currentUri);
-			return request;
-		}
-
 	}
 
 }
--- samples/oauth2-tonr/src/main/java/org/springframework/security/oauth/examples/config/WebMvcConfig.java
+++ samples/oauth2-tonr/src/main/java/org/springframework/security/oauth/examples/config/WebMvcConfig.java
@@ -4,21 +4,35 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.context.support.ConversionServiceFactoryBean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.BufferedImageHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.security.oauth.examples.tonr.SparklrService;
 import org.springframework.security.oauth.examples.tonr.converter.AccessTokenRequestConverter;
 import org.springframework.security.oauth.examples.tonr.impl.SparklrServiceImpl;
 import org.springframework.security.oauth.examples.tonr.mvc.FacebookController;
 import org.springframework.security.oauth.examples.tonr.mvc.SparklrController;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.AccessTokenRequest;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.common.AuthenticationScheme;
+import org.springframework.security.oauth2.config.annotation.web.configuration.OAuth2ClientConfiguration;
 import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.servlet.View;
@@ -36,79 +50,155 @@ import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
-    public PropertySourcesPlaceholderConfigurer myPropertySourcesPlaceholderConfigurer() {
-        PropertySourcesPlaceholderConfigurer p = new PropertySourcesPlaceholderConfigurer();
-        p.setLocation(new ClassPathResource("sparklr.properties"));
-        return p;
-    }
+	public PropertySourcesPlaceholderConfigurer myPropertySourcesPlaceholderConfigurer() {
+		PropertySourcesPlaceholderConfigurer p = new PropertySourcesPlaceholderConfigurer();
+		p.setLocation(new ClassPathResource("sparklr.properties"));
+		return p;
+	}
 
-    @Bean
-    public ContentNegotiatingViewResolver contentViewResolver() throws Exception {
-        ContentNegotiatingViewResolver contentViewResolver = new ContentNegotiatingViewResolver();
-        ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
-        contentNegotiationManager.addMediaType("json", MediaType.APPLICATION_JSON);
-        contentViewResolver.setContentNegotiationManager(contentNegotiationManager.getObject());
-        contentViewResolver.setDefaultViews(Arrays.<View>asList(new MappingJacksonJsonView()));
-        return contentViewResolver;
-    }
+	@Bean
+	public ContentNegotiatingViewResolver contentViewResolver() throws Exception {
+		ContentNegotiatingViewResolver contentViewResolver = new ContentNegotiatingViewResolver();
+		ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
+		contentNegotiationManager.addMediaType("json", MediaType.APPLICATION_JSON);
+		contentViewResolver.setContentNegotiationManager(contentNegotiationManager.getObject());
+		contentViewResolver.setDefaultViews(Arrays.<View> asList(new MappingJacksonJsonView()));
+		return contentViewResolver;
+	}
 
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/jsp/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
 
-    @Override
-    public void configureDefaultServletHandling(
-            DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
 
-    @Bean
-    public SparklrController sparklrController(SparklrService sparklrService) {
-        SparklrController controller = new SparklrController();
-        controller.setSparklrService(sparklrService);
-        return controller;
-    }
+	@Bean
+	public SparklrController sparklrController(SparklrService sparklrService) {
+		SparklrController controller = new SparklrController();
+		controller.setSparklrService(sparklrService);
+		return controller;
+	}
 
-    @Bean
-    public FacebookController facebookController(@Qualifier("facebookRestTemplate") RestOperations facebookRestTemplate) {
-        FacebookController controller = new FacebookController();
-        controller.setFacebookRestTemplate(facebookRestTemplate);
-        return controller;
-    }
+	@Bean
+	public FacebookController facebookController(@Qualifier("facebookRestTemplate")
+	RestOperations facebookRestTemplate) {
+		FacebookController controller = new FacebookController();
+		controller.setFacebookRestTemplate(facebookRestTemplate);
+		return controller;
+	}
 
-    @Bean
-    public SparklrServiceImpl sparklrService(@Value("${sparklrPhotoListURL}") String sparklrPhotoListURL,
-            @Value("${sparklrPhotoURLPattern}") String sparklrPhotoURLPattern,
-            @Value("${sparklrTrustedMessageURL}") String sparklrTrustedMessageURL,
-            @Qualifier("sparklrRestTemplate") RestOperations sparklrRestTemplate,
-            @Qualifier("trustedClientRestTemplate") RestOperations trustedClientRestTemplate) {
-        SparklrServiceImpl sparklrService = new SparklrServiceImpl();
-        sparklrService.setSparklrPhotoListURL(sparklrPhotoListURL);
-        sparklrService.setSparklrPhotoURLPattern(sparklrPhotoURLPattern);
-        sparklrService.setSparklrTrustedMessageURL(sparklrTrustedMessageURL);
-        sparklrService.setSparklrRestTemplate(sparklrRestTemplate);
-        sparklrService.setTrustedClientRestTemplate(trustedClientRestTemplate);
-        return sparklrService;
-    }
+	@Bean
+	public SparklrServiceImpl sparklrService(@Value("${sparklrPhotoListURL}")
+	String sparklrPhotoListURL, @Value("${sparklrPhotoURLPattern}")
+	String sparklrPhotoURLPattern, @Value("${sparklrTrustedMessageURL}")
+	String sparklrTrustedMessageURL, @Qualifier("sparklrRestTemplate")
+	RestOperations sparklrRestTemplate, @Qualifier("trustedClientRestTemplate")
+	RestOperations trustedClientRestTemplate) {
+		SparklrServiceImpl sparklrService = new SparklrServiceImpl();
+		sparklrService.setSparklrPhotoListURL(sparklrPhotoListURL);
+		sparklrService.setSparklrPhotoURLPattern(sparklrPhotoURLPattern);
+		sparklrService.setSparklrTrustedMessageURL(sparklrTrustedMessageURL);
+		sparklrService.setSparklrRestTemplate(sparklrRestTemplate);
+		sparklrService.setTrustedClientRestTemplate(trustedClientRestTemplate);
+		return sparklrService;
+	}
 
-    @Bean
-    public ConversionServiceFactoryBean conversionService() {
-        ConversionServiceFactoryBean conversionService = new ConversionServiceFactoryBean();
-        conversionService.setConverters(Collections.singleton(new AccessTokenRequestConverter()));
-        return conversionService;
-    }
+	@Bean
+	public ConversionServiceFactoryBean conversionService() {
+		ConversionServiceFactoryBean conversionService = new ConversionServiceFactoryBean();
+		conversionService.setConverters(Collections.singleton(new AccessTokenRequestConverter()));
+		return conversionService;
+	}
 
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-    }
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
 
-    @Override
-    public void configureMessageConverters(
-            List<HttpMessageConverter<?>> converters) {
-        converters.add(new BufferedImageHttpMessageConverter());
-    }
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new BufferedImageHttpMessageConverter());
+	}
+
+	@Configuration
+	@Import(OAuth2ClientConfiguration.class)
+	protected static class ResourceConfiguration {
+
+		@Value("${accessTokenUri}")
+		private String accessTokenUri;
+
+		@Value("${userAuthorizationUri}")
+		private String userAuthorizationUri;
+
+		@Resource
+		@Qualifier("accessTokenRequest")
+		private AccessTokenRequest accessTokenRequest;
+
+		@Bean
+		public OAuth2ProtectedResourceDetails sparklr() {
+			AuthorizationCodeResourceDetails details = new AuthorizationCodeResourceDetails();
+			details.setId("sparklr/tonr");
+			details.setClientId("tonr");
+			details.setClientSecret("secret");
+			details.setAccessTokenUri(accessTokenUri);
+			details.setUserAuthorizationUri(userAuthorizationUri);
+			details.setScope(Arrays.asList("read", "write"));
+			return details;
+		}
+
+		@Bean
+		public OAuth2ProtectedResourceDetails facebook() {
+			AuthorizationCodeResourceDetails details = new AuthorizationCodeResourceDetails();
+			details.setId("facebook");
+			details.setClientId("233668646673605");
+			details.setClientSecret("33b17e044ee6a4fa383f46ec6e28ea1d");
+			details.setAccessTokenUri("https://graph.facebook.com/oauth/access_token");
+			details.setUserAuthorizationUri("https://www.facebook.com/dialog/oauth");
+			details.setTokenName("oauth_token");
+			details.setAuthenticationScheme(AuthenticationScheme.query);
+			details.setClientAuthenticationScheme(AuthenticationScheme.form);
+			return details;
+		}
+
+		@Bean
+		public OAuth2ProtectedResourceDetails trusted() {
+			ClientCredentialsResourceDetails details = new ClientCredentialsResourceDetails();
+			details.setId("sparklr/trusted");
+			details.setClientId("my-client-with-registered-redirect");
+			details.setAccessTokenUri(accessTokenUri);
+			details.setScope(Arrays.asList("trust"));
+			return details;
+		}
+
+		@Bean
+		@Scope(value = "session", proxyMode = ScopedProxyMode.INTERFACES)
+		public OAuth2RestTemplate facebookRestTemplate() {
+			OAuth2RestTemplate template = new OAuth2RestTemplate(facebook(), new DefaultOAuth2ClientContext(
+					accessTokenRequest));
+			MappingJacksonHttpMessageConverter converter = new MappingJacksonHttpMessageConverter();
+			converter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON,
+					MediaType.valueOf("text/javascript")));
+			template.setMessageConverters(Arrays.<HttpMessageConverter<?>> asList(converter));
+			return template;
+		}
+
+		@Bean
+		@Scope(value = "session", proxyMode = ScopedProxyMode.INTERFACES)
+		public OAuth2RestTemplate sparklrRestTemplate() {
+			return new OAuth2RestTemplate(sparklr(), new DefaultOAuth2ClientContext(accessTokenRequest));
+		}
+
+		@Bean
+		public OAuth2RestTemplate trustedClientRestTemplate() {
+			return new OAuth2RestTemplate(trusted(), new DefaultOAuth2ClientContext());
+		}
+
+	}
+
 }
